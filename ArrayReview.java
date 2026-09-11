d8190b2db622611b7d61be39f55697f7118754e6
Tiveron <dtiveron@dist113.org>
2026-09-11 09:56:10 -0500

    Added 2D Arrays

--- ArrayReview.java
+++ ArrayReview.java
@@ -29,5 +29,35 @@ public class ArrayReview{
             System.out.print(g + " ");
         }
         System.out.println();
+
+
+        //A 2D array is actually an array of arrays. 
+        //It is a collection of rows and columns.
+
+        int[][] values = { {1, 2, 3},
+                           {4, 5, 6}, 
+                           {7, 8, 9}  
+                         };
+        
+        double[][] nums = new double[3][5];
+        
+        int total = 0;
+        for(int r = 0; r < values.length; r++){
+            for(int c = 0; c < values[r].length; c++){
+                System.out.print(values[r][c] + " ");
+                total += values[r][c];
+            }
+            System.out.println();
+        }
+        System.out.println("Total: " + total);
+
+        for(double[] row : nums){
+            for(double n : row){
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
